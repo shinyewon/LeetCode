ffe9ecdf53349e19f087b77a1335ddda09a333c6
Yewon Shin <rac8793@naver.com>
2023-07-22 14:13:02 +0900

    Time: 20 ms (62.70%), Space: 13.5 MB (72.78%) - LeetHub

---
+++ 0078-subsets/0078-subsets.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def subsets(self, nums):
+        res = []
+        self.dfs(nums, [], res)
+        return res
+    
+    def dfs(self, nums, path, res):
+        res.append(path)
+        for i in range(len(nums)):
+            self.dfs(nums[i+1:], path + [nums[i]], res)
